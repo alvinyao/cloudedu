5d2884c8ebc5043fb53d4b828f757362f9d9eae7
Alvin Yao <yao.angellin@gmail.com>
2013-08-28 01:03:03 +0800

    update menu

--- crud/views.py
+++ crud/views.py
@@ -8,15 +8,70 @@ from django_tables2.views import SingleTableView
 
 class CreateView( generic.CreateView ):
 	template_name = 'crud/plain/form.html'
+	app  = 'res'
+	curr = 'datadict'
+
+	def get_context_data(self, **kwargs):
+		context = super(CreateView, self).get_context_data()
+		if hasattr(self, 'app'):
+			context['app'] = self.app
+		if hasattr(self, 'curr'):
+			context['curr'] = self.curr
+		return context
+
 
 class UpdateView( generic.UpdateView ):
 	template_name = 'crud/plain/form.html'
+	app  = 'res'
+	curr = 'datadict'
+
+	def get_context_data(self, **kwargs):
+		context = super(UpdateView, self).get_context_data(**kwargs)
+		if hasattr(self, 'app'):
+			context['app'] = self.app
+		if hasattr(self, 'curr'):
+			context['curr'] = self.curr
+		return context
+
 
 class ListView( SingleTableView ):
 	template_name = "crud/plain/list.html"
+	app  = 'res'
+	curr = 'datadict'
+
+	def get_context_data(self, **kwargs):
+		context = super(ListView, self).get_context_data(**kwargs)
+		if hasattr(self, 'app'):
+			context['app'] = self.app
+		if hasattr(self, 'curr'):
+			context['curr'] = self.curr
+		return context
+
 
 class DetailView( generic.DetailView ):
 	template_name = 'crud/plain/detail.html'
+	app  = 'res'
+	curr = 'datadict'
+
+	def get_context_data(self, **kwargs):
+		context = super(DetailView, self).get_context_data(**kwargs)
+		if hasattr(self, 'app'):
+			context['app'] = self.app
+		if hasattr(self, 'curr'):
+			context['curr'] = self.curr
+		return context
+
 
 class DeleteView( generic.DeleteView ):
 	template_name = 'crud/plain/confirm_delete.html'
+	app  = 'res'
+	curr = 'datadict'
+
+	def get_context_data(self, **kwargs):
+		context = super(DeleteView, self).get_context_data(**kwargs)
+		if hasattr(self, 'app'):
+			context['app'] = self.app
+		if hasattr(self, 'curr'):
+			context['curr'] = self.curr
+		return context
+
--- essential/views.py
+++ essential/views.py
@@ -4,15 +4,25 @@ from tables import DataDictTable
 
 class CreateView(crud_views.CreateView):
 	model = DataDict
+	app = 'basic'
+	curr = 'datadict'
 
 class DetailView(crud_views.DetailView):
 	model = DataDict
+	app = 'basic'
+	curr = 'datadict'
 
 class DeleteView(crud_views.DeleteView):
 	model = DataDict
+	app = 'basic'
+	curr = 'datadict'
 
 class ListView(crud_views.ListView):
 	model = DataDict
 	queryset = DataDict.objects.all()
 	table_class = DataDictTable
 	table_pagination = {'per_page': 10}
+	template_name = "essential/list.html"
+
+	app = 'basic'
+	curr = 'datadict'
